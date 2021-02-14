e6530683817c358d73b801da862065aba0d17786
Sarthak Kapoor <sarthak.kapoor@joshtechnologygroup.com>
2021-02-15 03:10:26 +0530

    scheduled app structure changed

--- clock.py
+++ clock.py
@@ -2,13 +2,12 @@ from datetime import datetime, timedelta
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from endpoints import models as endpoints_models
-
 scheduler = BackgroundScheduler()
 
 
 @scheduler.scheduled_job('interval', minutes=1)
 def timed_job():
+    from endpoints import models as endpoints_models
     endpoints_models.Endpoint.objects.filter(created_at__lte=datetime.now() - timedelta(minutes=1)).delete()
 
 
