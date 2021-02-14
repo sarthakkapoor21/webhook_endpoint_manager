c205f6d767ce32ee96a2d9afb039f79ad481c28c
Sarthak Kapoor <sarthak.kapoor@joshtechnologygroup.com>
2021-02-14 16:54:54 +0530

    DB changed to Postgres for Heroku

--- webhook_endpoint_manager/settings.py
+++ webhook_endpoint_manager/settings.py
@@ -94,13 +94,22 @@ WSGI_APPLICATION = 'webhook_endpoint_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wem',
+        'USER': 'postgres',
+        'PASSWORD': 'passpass',
+        'HOST': 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
