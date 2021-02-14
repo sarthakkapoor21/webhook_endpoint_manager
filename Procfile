5e5f625e91d96bc0cc3715a30a41747b3027680d
Sarthak Kapoor <sarthak.kapoor@joshtechnologygroup.com>
2021-02-15 02:59:41 +0530

    Add Scheduler to delete endpoints

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn webhook_endpoint_manager.wsgi --log-file -
+clock: python clock.py
---
+++ clock.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from endpoints import models as endpoints_models
+
+scheduler = BackgroundScheduler()
+
+
+@scheduler.scheduled_job('interval', minutes=1)
+def timed_job():
+    endpoints_models.Endpoint.objects.filter(created_at__gte=datetime.now() - timedelta(minutes=1)).delete()
+
+
+scheduler.start()
