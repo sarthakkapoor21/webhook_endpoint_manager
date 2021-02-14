fe442b95aee4721ee6af01bf6edddfa179bc70f3
Sarthak Kapoor <sarthak.kapoor@joshtechnologygroup.com>
2021-02-15 03:33:04 +0530

    Scheduler Settings

--- clock.py
+++ clock.py
@@ -7,13 +7,12 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 scheduler = BlockingScheduler()
 django.setup(set_prefix=False)
 
-from endpoints import models as endpoints_models
-
 
 @scheduler.scheduled_job('interval', minutes=1)
-def timed_job():
-    print('THIS RUNS EVERY MINUTE\n')
-    endpoints_models.Endpoint.objects.filter(created_at__lte=datetime.now() - timedelta(minutes=1)).delete()
+def delete_hour_old_endpoints():
+    print('THIS RUNS EVERY MINUTE TO DELETE HOUR OLD ENDPOINTS\n')
+    from endpoints import models as endpoints_models
+    endpoints_models.Endpoint.objects.filter(created_at__lte=datetime.now() - timedelta(minutes=60)).delete()
 
 
 scheduler.start()
